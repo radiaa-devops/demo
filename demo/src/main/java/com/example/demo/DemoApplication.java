e5adfd39b7733b745d920f23c2245480d3a29df3
radiaa-devops <radiaa.devops@gmail.com>
2023-06-14 13:18:54 +0300

    done

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -11,7 +11,7 @@ public class DemoApplication {
 
 	@GetMapping("/")
 	public String home() {
-		return "Hello World!";
+		return "Radhia work Spring boot dockerized";
 	}
 
 	public static void main(String[] args) {
